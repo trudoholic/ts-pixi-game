effb834dfd1065785458f9d76ea9c8bb1686e44d
Oleg <35769958+trudoholic@users.noreply.github.com>
2021-10-31 14:49:19 +0300

    add inter

--- src/pixi-view/PixiView.ts
+++ src/pixi-view/PixiView.ts
@@ -20,37 +20,91 @@ export default class PixiView {
 
     const view = this.app.view;
     document.body.appendChild(view);
-    this.test();
 
+    // tsconfig.json :: "esModuleInterop": true,
+    this.app.stage.name = "stage";
+    this.app.stage.interactive = true;
+    this.app.stage.hitArea = this.app.screen;
+    this.app.stage.on('pointerup', this.onPointerUp);
+
+    // this.test();
+    this.hit_test();
   }
 
-    /**
-     * test
-     */
-    public test() {
-      const container = new PIXI.Container();
-      this.app.stage.addChild(container);
+  /**
+   * onPointerUp
+   */
+  public onPointerUp(event: PIXI.InteractionEvent) {
+    event.stopPropagation();
+    // const e = event.data.originalEvent;
+    // if (e) {
+      // const target = e.target as PIXI.Container;
+      // const currentTarget = e.currentTarget as PIXI.Container;
+      // console.log(">>", (e.target as any).name, (e.currentTarget as any).name);
+    // }
+    console.log(">>", event.target.name, event.currentTarget.name);
+  }
 
-      // const texture = PIXI.Texture.from('examples/assets/bunny.png');
-      const texture = PIXI.Texture.WHITE;
-      for (let i = 0; i < 25; i++) {
-          const bunny = new PIXI.Sprite(texture);
-          bunny.alpha = 0.5;
-          bunny.anchor.set(0.5);
-          bunny.x = (i % 5) * 40;
-          bunny.y = Math.floor(i / 5) * 40;
-          container.addChild(bunny);
-      }
+  /**
+   * hit_test
+   */
+  public hit_test() {
+    const container = new PIXI.Container();
+    container.name = "container";
+    this.app.stage.addChild(container);
 
-      container.x = this.app.screen.width / 2;
-      container.y = this.app.screen.height / 2;
-      container.pivot.x = container.width / 2;
-      container.pivot.y = container.height / 2;
+    container.interactive = true;
+    container.on('pointerup', this.onPointerUp);
 
-      this.app.ticker.add((delta) => {
-          container.rotation -= 0.01 * delta;
-      });
+    // const texture = PIXI.Texture.from('examples/assets/bunny.png');
+    const texture = PIXI.Texture.WHITE;
+    for (let i = 0; i < 25; i++) {
+        const bunny = new PIXI.Sprite(texture);
+        bunny.alpha = 0.5;
+        bunny.anchor.set(0.5);
+        bunny.x = (i % 5) * 40;
+        bunny.y = Math.floor(i / 5) * 40;
+        bunny.name = "bunny_" + i;
+        container.addChild(bunny);
+
+        bunny.interactive = true;
+        bunny.buttonMode  = true;
+        bunny.on('pointerup', this.onPointerUp);
     }
+
+    container.x = this.app.screen.width / 2;
+    container.y = this.app.screen.height / 2;
+    container.pivot.x = container.width / 2;
+    container.pivot.y = container.height / 2;
+  }
+
+  /**
+   * test
+   */
+  public test() {
+    const container = new PIXI.Container();
+    this.app.stage.addChild(container);
+
+    // const texture = PIXI.Texture.from('examples/assets/bunny.png');
+    const texture = PIXI.Texture.WHITE;
+    for (let i = 0; i < 25; i++) {
+        const bunny = new PIXI.Sprite(texture);
+        bunny.alpha = 0.5;
+        bunny.anchor.set(0.5);
+        bunny.x = (i % 5) * 40;
+        bunny.y = Math.floor(i / 5) * 40;
+        container.addChild(bunny);
+    }
+
+    container.x = this.app.screen.width / 2;
+    container.y = this.app.screen.height / 2;
+    container.pivot.x = container.width / 2;
+    container.pivot.y = container.height / 2;
+
+    this.app.ticker.add((delta) => {
+        container.rotation -= 0.01 * delta;
+    });
+  }
 
   /**
    * hello
