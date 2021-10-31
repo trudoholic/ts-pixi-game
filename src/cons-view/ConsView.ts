aa80a4f8ddb7e879c5198b75ab9e20afad400a13
Oleg <35769958+trudoholic@users.noreply.github.com>
2021-10-31 15:41:06 +0300

    add kb

---
+++ src/cons-view/ConsView.ts
@@ -0,0 +1,33 @@
+export default class ConsView {
+
+  constructor() {
+
+    document.addEventListener('keydown', event => {
+      if (event.repeat) {
+        event.preventDefault();
+        return;
+      }
+      console.log('KEYDOWN');
+      console.log('Alt: ', event.altKey);
+      console.log('Ctrl: ', event.ctrlKey);
+      console.log('Shift: ', event.shiftKey);
+    });
+
+    document.addEventListener('keypress', event => {
+      event.preventDefault();
+      if (event.repeat) { return; }
+      console.log('KEYPRESS');
+      console.log('ASCII код: ', event.charCode);
+    });
+
+    document.addEventListener('keyup', event => {
+      event.preventDefault();
+      console.log('KEYUP');
+      console.log('Key: ', event.key);
+      console.log('keyCode: ', event.keyCode);
+      console.log('Строковый код: ', event.code);
+    });
+
+  }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import Model from "./model/Model"
+import ConsView from "./cons-view/ConsView"
 import PixiView from "./pixi-view/PixiView"
 
 console.log("hello world!");
 
 const model = new Model();
-model.hello();
+// model.hello();
 
+const consView = new ConsView();
 const pixiView = new PixiView();
-pixiView.hello();
+// pixiView.hello();
