937af023a54fc0401cb9839dfe2e6b8178394a2c
Oleg <35769958+trudoholic@users.noreply.github.com>
2021-11-05 23:04:47 +0300

    add menu

--- src/pixi-view/HUD.ts
+++ src/pixi-view/HUD.ts
@@ -10,13 +10,21 @@ export default class HUD extends PIXI.Container {
     super();
     this.name = "HUD";
 
-    const panelA = new Panel(rect);
+    const panelA = new Panel(rect, { isVert: true });
     this.addChild(panelA);
-    config.players.forEach(it => panelA.addButton(it.name));
+    ['Draw', 'Play', 'Hand', 'Keep'].forEach(it => panelA.addButton(it));
 
-    const panelB = new Panel(rect, { isVert: true, dockX: .5, dockY: .5 });
+    const panelB = new Panel(rect, { isVert: true, dockX: 1 });
     this.addChild(panelB);
-    config.players.forEach(it => panelB.addButton(it.name));
+    ['Deck', 'Pile', 'Round', 'Player'].forEach(it => panelB.addButton(it));
+
+    const panelC = new Panel(rect, { dockX: .5 });
+    this.addChild(panelC);
+    ['New', 'End'].forEach(it => panelC.addButton(it));
+
+    const panelD = new Panel(rect, { dockX: .5, dockY: .5 });
+    this.addChild(panelD);
+    config.players.forEach(it => panelD.addButton(it.name));
 
   }
 
